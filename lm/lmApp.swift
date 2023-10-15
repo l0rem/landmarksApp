7d6a1a5df410ddbf16356742cdc58804905db488
l0rem <nikita@fastdocs.de>
2023-10-15 21:35:37 +0200

    Initial Commit

---
+++ lm/lmApp.swift
@@ -0,0 +1,17 @@
+//
+//  lmApp.swift
+//  lm
+//
+//  Created by Nik Lorem on 15.10.23.
+//
+
+import SwiftUI
+
+@main
+struct lmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
